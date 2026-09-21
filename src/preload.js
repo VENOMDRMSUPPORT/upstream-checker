4af4c8b9f9be3703fae84a2eb984d76c9d723d4a
VENOM DRM SUPPORT <support@venom-drm.com>
2026-09-21 21:33:36 +0300

    feat: expose update API to renderer via preload

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,37 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // Window controls
   minimize: () => ipcRenderer.send('window-minimize'),
   maximize: () => ipcRenderer.send('window-maximize'),
   close: () => ipcRenderer.send('window-close'),
+
+  // API requests
   apiRequest: (opts) => ipcRenderer.invoke('api-request', opts),
   openExternal: (url) => ipcRenderer.send('open-external', url),
+
+  // Update API
+  updateAPI: {
+    onUpdateChecking: (callback) => {
+      ipcRenderer.on('update-checking', () => callback());
+    },
+    onUpdateAvailable: (callback) => {
+      ipcRenderer.on('update-available', (_, info) => callback(info));
+    },
+    onUpdateNotAvailable: (callback) => {
+      ipcRenderer.on('update-not-available', () => callback());
+    },
+    onUpdateError: (callback) => {
+      ipcRenderer.on('update-error', (_, data) => callback(data));
+    },
+    onDownloadProgress: (callback) => {
+      ipcRenderer.on('update-download-progress', (_, progress) => callback(progress));
+    },
+    onUpdateDownloaded: (callback) => {
+      ipcRenderer.on('update-downloaded', (_, info) => callback(info));
+    },
+    downloadUpdate: () => ipcRenderer.send('download-update'),
+    installUpdate: () => ipcRenderer.send('install-update'),
+    checkForUpdates: () => ipcRenderer.send('check-for-updates-manual'),
+  },
 });
